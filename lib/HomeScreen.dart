568319d9f58527e4e49a0ec40e09d8deb256e5fd
nour ben bechir <nourbechir203@gmail.com>
2025-01-22 19:36:14 +0100

    first

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,40 +1,50 @@
 import 'package:flutter/material.dart';
+
 //stless
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Color(0xff182020),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(left: 15.0,right: 15),
+          padding: const EdgeInsets.only(left: 15.0, right: 15),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text('Messages ',style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontFamily:('Quicksand') ,
-                    fontSize: 30,
-                    color: Colors.white
-                  ),),
-                  IconButton(onPressed: (){},
-                    icon:const Icon(Icons.search,
-                    color: Colors.white,
-                    size: 36,
-                    ))
-                ],),
-              const SizedBox(height: 5,),
-              Text('R E C E N T ',style: TextStyle(
-                color: Colors.white,
-              ),),
+                  const Text(
+                    'Messages ',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: ('Quicksand'),
+                        fontSize: 30,
+                        color: Colors.white),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: 36,
+                      ))
+                ],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                'R E C E N T NB DJJSDKSL SCIKPl ',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
               SizedBox(
-                height:15 ,
+                height: 15,
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -43,24 +53,27 @@ class Homescreen extends StatelessWidget {
                   CircleAvatar(
                     radius: 30,
                     backgroundImage:
-                    Image.asset('assets/images/img1.jpg').image,
+                        Image.asset('assets/images/img1.jpg').image,
                   ),
-                  SizedBox(height: 10,),
-                  Text('Ahmed achour',style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-              ),)
-                
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Ahmed achour',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 200,
+                    ),
+                  )
                 ],
               ),
-              SizedBox(height: 10,),
-              
-
+              SizedBox(
+                height: 10,
+              ),
             ],
           ),
         ),
       ),
     );
-
   }
 }
