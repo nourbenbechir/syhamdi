f673fa06091aeb5ad19c4f45b45852c3e0a66a7c
nour ben bechir <nourbechir203@gmail.com>
2025-01-22 19:06:53 +0100

    first commit

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+//stless
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Color(0xff182020),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15.0,right: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Messages ',style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily:('Quicksand') ,
+                    fontSize: 30,
+                    color: Colors.white
+                  ),),
+                  IconButton(onPressed: (){},
+                    icon:const Icon(Icons.search,
+                    color: Colors.white,
+                    size: 36,
+                    ))
+                ],),
+              const SizedBox(height: 5,),
+              Text('R E C E N T ',style: TextStyle(
+                color: Colors.white,
+              ),),
+              SizedBox(
+                height:15 ,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage:
+                    Image.asset('assets/images/img1.jpg').image,
+                  ),
+                  SizedBox(height: 10,),
+                  Text('Ahmed achour',style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),)
+                
+                ],
+              ),
+              SizedBox(height: 10,),
+              
+
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+}
